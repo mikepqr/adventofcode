c55e8c8a1d99f8bd409883a430d1b2a91d3dd5b1
Mike Lee Williams <mike@mike.place>
2017-12-03 15:56:09 -0800

    Simplify solution

--- 03/manhattan.py
+++ 03/manhattan.py
@@ -1,53 +1,29 @@
 import itertools
 
 
-def inside(n):
-    """Tiles inside nth shell."""
-    return set(range(1, (2*n+1)**2 + 1)) if n >= 0 else set()
-
-
-def perimeter(n):
-    """Tiles in nth shell."""
-    return inside(n) - inside(n-1)
-
-
-def min_shell(n):
-    """Smallest tile in nth shell."""
-    return min(perimeter(n))
-
-
-def shell(k):
-    """Shell containing k."""
-    for n in itertools.count():
-        if k in inside(n):
-            return n
-
-
-def side_length(n):
-    """Length of sides of nth shell."""
-    return 2*n + 1
-
-
-def middles(n):
-    """Tiles at the middle of the nth shell."""
-    if n == 0:
-        return {}
-    else:
-        mn = min_shell(n)
-        sd = side_length(n)
-        rhs_mid = mn + n - 1
-        return set(rhs_mid + i*(sd-1) for i in (0, 1, 2, 3))
-
-
 def manhattan(k):
     """Manhattan distance from k to 1."""
-    if k == 1:
-        return 0
-    else:
-        n = shell(k)
-        mds = middles(n)
-        return n + min([abs(k - md) for md in mds] +
-                       [abs(md - k) for md in mds])
+    coord = list(itertools.islice(spiral(), k))[-1]
+    return abs(coord[0]) + abs(coord[1])
+
+
+def spiral(center=(0, 0)):
+    """Iterator over coordinates of spiral."""
+    x, y = center
+    yield x, y
+    for n in itertools.count():
+        while y < n:
+            y += 1
+            yield x, y
+        while x > -n:
+            x -= 1
+            yield x, y
+        while y > -n:
+            y -= 1
+            yield x, y
+        while x < n + 1:
+            x += 1
+            yield x, y
 
 
 def part1():
--- 03/test_manhattan.py
+++ 03/test_manhattan.py
@@ -1,32 +1,6 @@
 import manhattan
 
 
-def test_perimeter():
-    assert manhattan.perimeter(0) == {1}
-    assert manhattan.perimeter(1) == {2, 3, 4, 5, 6, 7, 8, 9}
-
-
-def test_side_length():
-    assert manhattan.side_length(0) == 1
-    assert manhattan.side_length(1) == 3
-    assert manhattan.side_length(2) == 5
-
-
-def test_shell():
-    assert manhattan.shell(1) == 0
-    for i in range(2, 10):
-        assert manhattan.shell(i) == 1
-    for i in (10, 15, 17, 21, 23):
-        assert manhattan.shell(i) == 2
-
-
-def test_middles():
-    assert manhattan.middles(0) == {}
-    assert manhattan.middles(1) == {2, 4, 6, 8}
-    assert manhattan.middles(2) == {11, 15, 19, 23}
-    assert manhattan.middles(3) == {28, 34, 40, 46}
-
-
 def test_manhattan():
     assert manhattan.manhattan(1) == 0
     assert manhattan.manhattan(12) == 3
