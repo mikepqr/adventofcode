1ec48407acdd680f126b278dde8d0b76ac75b8e0
Mike Lee Williams <mike@mike.place>
2022-12-14 23:27:51 -0800

    Correct solution for 2022/15 part 2 (still too slow)

--- 2022/15/beacons.py
+++ 2022/15/beacons.py
@@ -45,31 +45,17 @@ def compute1(fname, y=10):
     return len(excluded)
 
 
+def discovered_by_sensor(x, y, s):
+    return abs(s.sx - x) + abs(s.sy - y) <= abs(s.sx - s.bx) + abs(s.sy - s.by)
+
+
 def compute2(fname, space=20):
     sensors = parse_input(fname)
     for y in range(space + 1):
-        possible = set(range(space + 1))
-        excluded = set()
-        for sensor in sensors:
-            excluded.update(set(ysection(sensor, y)))
-            if sensor.sy == y and sensor.sx in excluded:
-                excluded.remove(sensor.sx)
-            if sensor.by == y and sensor.bx in excluded:
-                excluded.remove(sensor.bx)
-        possible.difference_update(excluded)
-        if possible:
-            print(y, possible)
-
-    return list(possible)[0] * 4000000 + y
-
-
-def compute2_idea(fname, space):
-    # for every point in the grid
-    # for every sensor
-    # is that point closer to the sensor than its beacon
-    # if so, it cannot be where the undiscovered beacon is hiding
-    # try the next point
-    pass
+        for x in range(space + 1):
+            discovered = any(discovered_by_sensor(x, y, s) for s in sensors)
+            if not discovered:
+                return x * 4000000 + y
 
 
 def test_compute1():
