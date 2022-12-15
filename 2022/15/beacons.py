c16f6475a190532d6403a5df04eac6993083d96b
Mike Lee Williams <mike@mike.place>
2022-12-15 15:24:11 -0800

    Add final working solution for 2022/15 part 2

--- 2022/15/beacons.py
+++ 2022/15/beacons.py
@@ -26,40 +26,84 @@ def parse_input(fname):
     return sensors
 
 
-def ysection(sensor, y):
-    d = abs(sensor.sx - sensor.bx) + abs(sensor.sy - sensor.by)
-    dx = d - abs(sensor.sy - y)
-    return range(sensor.sx - dx, sensor.sx + dx + 1)
+def ysections(sensors, y) -> list[tuple[int, int]]:
+    """
+    Returns a list of the [) boundaries of the zone explored by a list of
+    sensors a y=y.
+
+    Not all sensors explore y=y, this list may be shorter than len(sensors).
+
+                   1    1    2    2
+         0    5    0    5    0    5
+    -2 ..........#.................
+    -1 .........###................
+     0 ....S...#####...............
+     1 .......#######........S.....
+     2 ......#########S............
+     3 .....###########SB..........
+     4 ....#############...........
+     5 ...###############..........
+     6 ..#################.........
+     7 .#########S#######S#........
+     8 ..#################.........
+     9 ...###############..........
+    10 ....B############...........
+    11 ..S..###########............
+    12 ......#########.............
+    13 .......#######..............
+    14 ........#####.S.......S.....
+    15 B........###................
+    16 ..........#SB...............
+    17 ................S..........B
+    18 ....S.......................
+    19 ............................
+    20 ............S......S........
+    21 ............................
+    22 .......................B....
+
+    e.g. the boundary for this sensor at y=10 is (2, 15)
+    """
+    boundaries = []
+    for sensor in sensors:
+        # Manhattan distance from sensor to its nearest beacon
+        d = abs(sensor.sx - sensor.bx) + abs(sensor.sy - sensor.by)
+        dx = d - abs(sensor.sy - y)
+        if dx >= 0:
+            left, right = sensor.sx - dx, sensor.sx + dx + 1
+            boundaries.append((left, right))
+    return boundaries
 
 
 def compute1(fname, y=10):
     sensors = parse_input(fname)
     excluded = set()
+    boundaries = ysections(sensors, y)
+    for boundary in boundaries:
+        excluded.update(set(range(*boundary)))
     for sensor in sensors:
-        excluded.update(set(ysection(sensor, y)))
-        if sensor.sy == y and sensor.sx in excluded:
-            excluded.remove(sensor.sx)
         if sensor.by == y and sensor.bx in excluded:
             excluded.remove(sensor.bx)
-
     return len(excluded)
-
-
-def discovered_by_sensor(x, y, s):
-    return abs(s.sx - x) + abs(s.sy - y) <= abs(s.sx - s.bx) + abs(s.sy - s.by)
 
 
 def compute2(fname, space=20):
     sensors = parse_input(fname)
     for y in range(space + 1):
-        excluded = set()
-        for sensor in sensors:
-            excluded.update(set(ysection(sensor, y)))
-        possible = set(range(0, space + 1)).difference(excluded)
-        if possible:
-            break
-
-    return list(possible)[0] * 4000000 + y
+        boundaries = ysections(sensors, y)
+        # Sort boundaries by their left
+        boundaries.sort()
+        # Starting at the right of the 0th boundary
+        x = boundaries[0][1]
+        for i in range(1, len(boundaries)):
+            # If the next boundary overlaps with the current location
+            if boundaries[i][0] <= x:
+                # Step to the right of the next boundary if it is right of the
+                # current location
+                x = max(x, boundaries[i][1])
+            # If the next boundary does not overlap with the current location
+            # then we found the gap
+            else:
+                return x * 4000000 + y
 
 
 def test_compute1():
